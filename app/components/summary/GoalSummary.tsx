98e103765b1f6be6bc5f5009aad4bb14d589519d
Song ji su <70136320+z1su@users.noreply.github.com>
2023-07-07 19:01:01 +0900

    docs: Summary 관련 컴포넌트 추가

---
+++ app/components/summary/GoalSummary.tsx
@@ -0,0 +1,7 @@
+export default function GoalSummary() {
+  return (
+    <div>
+      <div>GoalSummaryComponent</div>
+    </div>
+  );
+}
---
+++ app/components/summary/MonthlySummary.tsx
@@ -0,0 +1,3 @@
+export default function MonthlySummary() {
+  return <div>MonthlySummaryComponent</div>;
+}
