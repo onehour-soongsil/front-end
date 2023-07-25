a1cde0385a3470dcc6b174b34a6d2b6e361d8784
unknown <doosan1224@naver.com>
2023-07-25 23:48:17 +0900

    feature : 회원가입 기능구현
    mongoDB의 API를 도와주는 helper 함수 구현
    회원가입 기능 구현
    - 이미 가입된 회원인 경우 오류를 보냄

---
+++ helper/db-utils.js
@@ -0,0 +1,79 @@
+import { MongoClient } from "mongodb";
+
+export async function connectDatabase() {
+  try {
+    const client = await MongoClient.connect(process.env.NEXT_PUBLIC_MONGODB_CONNECT_DB);
+    return client;
+  } catch (error) {
+    throw new Error("클라이언트 연결에 실패했습니다...😱");
+  }
+}
+
+export async function insertDocument(collection, document) {
+  const client = await connectDatabase();
+  const db = client.db();
+  const result = await db.collection(collection).insertOne(document);
+  client.close();
+  return result;
+}
+
+// 이메일로 유저 찾기
+export async function findUserByEmail(email) {
+  const client = await connectDatabase();
+  const db = client.db();
+  const user = await db.collection("users").findOne({ email });
+
+  client.close();
+  return user;
+
+  // export async function getAllDocuments(collection) {
+  //   const client = await connectDatabase();
+  //   const db = client.db();
+
+  //   let AllDocuments = await db.collection(collection).find().toArray();
+
+  //   AllDocuments = AllDocuments.map(post => {
+  //     post._id = post._id.toString();
+  //     return post;
+  //   });
+
+  //   client.close();
+  //   return AllDocuments;
+  // }
+
+  // export async function getSelectedDocuments(collection, postId) {
+  //   const client = await connectDatabase();
+  //   const db = client.db();
+  //   const result = await db.collection(collection).findOne({ _id: new ObjectId(postId) });
+
+  //   if (result) {
+  //     result._id = result._id.toString();
+  //   }
+
+  //   client.close();
+  //   return result;
+  // }
+
+  // // edit
+  // export async function replaceDocument(collection, selectedPostId, editPost) {
+  //   const client = await connectDatabase();
+  //   const db = client.db();
+  //   const result = await db
+  //     .collection(collection)
+  //     .updateOne({ _id: new ObjectId(selectedPostId) }, { $set: editPost });
+
+  //   client.close();
+  //   return result;
+  // }
+
+  // // delete
+  // export async function deleteSelectedDocument(collection, selectedPostId) {
+  //   const client = await connectDatabase();
+  //   const db = client.db();
+
+  //   const result = await db.collection(collection).deleteOne({ _id: new ObjectId(selectedPostId) });
+
+  //   client.close();
+  //   return result;
+  // }
+}
---
+++ pages/api/register.js
@@ -0,0 +1,17 @@
+import { findUserByEmail, insertDocument } from "../../helper/db-utils";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+      const user = await findUserByEmail(req.body.email);
+      if (user) {
+        throw new Error("이미 가입한 회원입니다.");
+      }
+
+      await insertDocument("users", req.body);
+      res.status(200).json("성공!");
+    } catch (error) {
+      res.status(400).json(error.message);
+    }
+  }
+}
