f75cac7f42128c98b59dfc967d7b66ffb3ae5f7e
unknown <doosan1224@naver.com>
2023-08-13 17:50:10 +0900

    feature : middleware 기능구현 로그인이 필요한 서비스를 로그인하지 않고 들어가려고 할 때 모두 login 페이지로 리다이렉팅

---
+++ app/no-data/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Empty } from "antd";
+
+export default function NoDataPage() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <Empty />
+    </div>
+  );
+}
